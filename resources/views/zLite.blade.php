d7514bab1bc92ea6cf7dca9b2d1a57640066b032
Aleksei <hello@aleksei.email>
2024-01-08 21:44:36 +0300

    fix 0801

---
+++ resources/views/zLite.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html id="html" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+	@include('_shema._lego.head.index')
+</head>
+<body id="body" class="@yield('css-body') @stack('css-body')">
+<script>
+	@include('_.js.bodyStart')
+	@stack('script-bodyStart')
+</script>
+
+@include('_shema._lego.input')
+
+<div id="Fon" class="Abs -all @stack('css-fon')">@stack('fon')</div>
+
+
+@stack('10111703')
+{{-- @include('_._skeleton.Top') --}}
+{{-- @include('_._skeleton.header') --}}
+{{-- @include('_._skeleton.menuMain') --}}
+@stack('menu')
+
+@yield('breadBack')
+@yield('head')
+
+@component('_wrap.index', ['id' => 'Lite', 'css' => 'Hero-padding HWin'])
+	<!-- slot('bar') -->
+	<!-- yield('bread') -->
+
+	@stack('topBefore')
+	@yield('top')
+	@stack('topAfter')
+
+	@yield('bread')
+
+	@stack('contentBefore')
+	@yield('content')
+
+	@stack('bottomBefore')
+	@stack('bottom')
+	@stack('bottomAfter')
+
+@endcomponent
+
+
+
+<div id="Milk" class="Abs Milk / index owlFIX">
+	@stack('milk')
+	<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+		@include('_.src.svg.index')
+		@stack('svg')
+	</svg>
+</div>
+
+@include('_._skeleton.Pop')
+
+<div id="Veil" data-click data-foo='click' data-goto></div>
+
+@include('_._skeleton.bodyEnd')
+@include('_shema._lego.afterMilk')
+
+</body>
+</html>
