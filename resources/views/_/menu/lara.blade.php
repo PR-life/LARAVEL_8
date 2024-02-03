a662a95d0c9a6d4177ef6ab235f2c144cffdda64
email <hello@aleksei.email>
2024-02-03 15:23:51 +0300

    fix Email 0302

--- resources/views/_/menu/lara.blade.php
+++ resources/views/_/menu/lara.blade.php
@@ -1,4 +1,4 @@
-<li><a href="{{route('morda')}}">Laravel</a></li>
-<li><a href="{{route('lara.categories')}}">Категории</a></li>
-<li><a href="{{route('lara.posts')}}">Посты</a></li>
+<li><a href="{-{route('morda')}}">Laravel</a></li>
+<li><a href="{-{route('lara.categories')}}">Категории</a></li>
+<li><a href="{-{route('lara.posts')}}">Посты</a></li>
 <li><a href="/">Paper</a></li>
