74148dd5f41b6754d263b4a243962ccd247c0713
Aleksei <oloid@aleksei.email>
2024-09-15 14:32:32 +0300

    cloud

--- resources/views/_CLOUD/xAsgrupp/_skeleton/Header.blade.php
+++ resources/views/_CLOUD/xAsgrupp/_skeleton/Header.blade.php
@@ -1,5 +1,5 @@
 <header id="Header" class="Goo / Abs_mbl index">
-	<div class="Box / Header -h_PC / flex cI_PC bC_PC / edge-this-r relative">
+	<div class="Box Edge -PC only-right / Header -h_PC / flex cI_PC bC_PC / relative" style="background: red">
 		<div class="Article W-100 / flex cI cC_mbl / cross_PC">
 			<a class="x-ava flex none_mbl block_morda / Shift -left -stepS / relative" href="/">
 				<span class="Abs -t -left / pr-2 x-opVersa none_Beyond none_mbl">
