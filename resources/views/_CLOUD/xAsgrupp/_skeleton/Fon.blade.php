f240a5d74b7f4d38d018e9fad561a069143f3144
Asgrupp <asgrupp@aleksei.email>
2024-09-16 17:12:19 +0300

    fix

---
+++ resources/views/_CLOUD/xAsgrupp/_skeleton/Fon.blade.php
@@ -0,0 +1,17 @@
+<div id="Fon" class="Abs -all / index noEvents @stack('css-fon')">
+	<!--googleoff: all-->
+	<!--noindex-->
+	<div class="x_29081724"></div>
+	<div class="Abs x_white x-top"></div>
+	<div class="Abs x_white x-bottom"></div>
+
+	<div class="_bg x-shadow / relative">
+		{{-- <div class="Top -h / none_mbl none_beyond"></div> --}}
+		{{-- <div class="Header -h / none_mbl"></div> --}}
+		@yield('fon')@stack('fon')
+	</div>
+	 
+	<!--/noindex-->
+	<!--googleon: all-->
+</div>
+ 
