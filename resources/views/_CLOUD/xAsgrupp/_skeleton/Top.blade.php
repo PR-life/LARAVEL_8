ce410577c0ffc4ab79918c2a395c1907ce85f5cd
Asgrupp <asgrupp@aleksei.email>
2024-09-16 13:34:50 +0300

    fix

--- resources/views/_CLOUD/xAsgrupp/_skeleton/Top.blade.php
+++ resources/views/_CLOUD/xAsgrupp/_skeleton/Top.blade.php
@@ -1,7 +1,7 @@
 <div class="paragraphX2 none Beyond"></div>
 <header id="Top" class="Wings_beyond noEvents / Abs_mbl -rt / none-beyond block_morda / edge-s">
 	<div class="_win">
-		<div class="Top -h / Box Edge -PC / flex eC_PC bC_mbl cI_mbl">
+		<div class="Top -h / Edge -PC only-right / flex eC_PC bC_mbl cI_mbl">
 			<div class="_left none_mbl flex_morda / flex cI_PC cross-m / yesEvents">
 				<div class="none_mbl / flex cross-m / Family-OpenSans -Grey">
 					@includeif('xAsgrupp._skeleton.lego.top_1')
