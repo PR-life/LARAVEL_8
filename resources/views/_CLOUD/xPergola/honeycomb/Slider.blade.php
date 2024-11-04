a81863e92460cfa0e9e4164fdf3f7614729fdbef
Asgrupp <asgrupp@aleksei.email>
2024-11-04 10:51:07 +0300

    fix 0411

--- resources/views/_CLOUD/xPergola/honeycomb/Slider.blade.php
+++ resources/views/_CLOUD/xPergola/honeycomb/Slider.blade.php
@@ -1,4 +1,85 @@
-<div id="Slider_prglsrvc" class="Box aura / Edge -SPC" style="opacity:1">
+<div id="Slider_prglsrvc" class="Pente aura / Edge -SPC">
+	<div class="Font -S / width-04111014">
+		<div class="content">Биоклиматические перголы</div>
+		<div class="paragraph"></div>
+		<div class="_h b500 / pergolservice-lineHeight">
+			Тень и уют <br />
+			в любой зоне.
+		</div>
+	</div>
+    <div class="hill-s"></div>
+	<div class="flex dC">
+		<div class="_tabs">
+			<div class="_wrap">
+				<div class="void-m">
+					<div class="_wrap">
+						<div class="void">
+							<div class="hMin b500">Частный дом и террасы</div>
+							{{-- <div class="content-s">Защитят от палящего солнца и дождя, позволяя быть на свежем воздухе в любую погоду.</div> --}}
+						</div>
+					</div>
+					<div class="_wrap">
+						<div class="void">
+							<div class="hMin b500">Кафе и рестораны</div>
+							{{-- <div class="content-s">Идеальное решение для создания комфортных зон отдыха на открытом воздухе.</div> --}}
+						</div>
+					</div>
+					<div class="_wrap">
+						<div class="void">
+							<div class="hMin b500">Гостиницы и курорты</div>
+							{{-- <div class="content-s">Идеально подходят для открытых террас, бассейнов и зон для завтраков на свежем воздухе.</div> --}}
+						</div>
+					</div>
+					<div class="_wrap">
+						<div class="void">
+							<div class="hMin b500">Общественные пространства</div>
+							{{-- <div class="content-s">Перголы могут использоваться в зонах отдыха парков и общественных мест.</div> --}}
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="_slider / Abs / round-s"></div>
+	</div>
+</div>
+
+
+
+<style>
+
+._tabs {
+    max-width: 500px;
+}
+
+
+ ._slider {
+    top: 0;
+    right: 0;
+    bottom: 0;
+    width: 50%;
+    background: #fafafc;
+ }
+    .width-04111014 {
+        max-width: 460px;
+    }
+ 
+    .pergolservice-lineHeight {
+        --Font-lineHeight:0px;
+
+    }
+</style>
+
+
+
+
+{{-- #Slider_prglsrvc {
+    .Plot .Bg{
+        background-image: url(/images/x_pergolservice/slider/01111207.jpg);
+    }
+} --}}
+
+{{-- <div id="Slider_prglsrvc" class="Box aura / Edge -SPC" style="opacity:1">
     <div class="flex">
         @include('_CLOUD.xPergola.honeycomb.lego.Slider.aside')
         <div class="Pente aura / Plot plot-h / relative">
@@ -7,8 +88,8 @@
 
                     <div class="item"></div>
                 </div>
-                {{-- <div class="Abs Ico Bg -gif / Visible" style="top:80%" visible-node='Slider' visible-param='Anime'></div> --}}
+                <div class="Abs Ico Bg -gif / Visible" style="top:80%" visible-node='Slider' visible-param='Anime'></div>
             </div>
          </div>
     </div>
-</div>
+</div> --}}
--- resources/views/mod/Stories/wrap/item.blade.php
+++ resources/views/mod/Stories/wrap/item.blade.php
@@ -1,12 +1,18 @@
-<div class="item Story / relative">
+<div class="item Story vol-{{$cssVol ?? ''}} / relative">
     @isset($img)
-    <img class="H-100 W-100 / Img -cover" src="{{$img}}" alt="55555">
+    <div class="Abs -all">
+        <img class="H-100 W-100 / Img -cover" src="{{$img}}" alt="55555">
+    </div>
     @endisset
     <div class="Abs -all / Bone / {{$textColor ?? '-Black'}} / index">
-        <div class="_head / story-bg_text {{$center ? 'center-text' : ''}}">
+        <div class="_head / story-bg_text {{isset($center) ? 'center-text' : ''}}">
+
+            @if(!isset($fullScreen))
             <div class="Top -h"></div>
             <div class="Header -h"></div>
-            @isset($img)
+            @endif
+
+            @isset($LLC)
             <div class="Wings">
                 <div class="_win -t / content b500">
                     {{$LLC}}
