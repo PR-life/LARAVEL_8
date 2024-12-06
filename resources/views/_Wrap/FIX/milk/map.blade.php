53db1502944d937cd12765cea134c2be022fb8f7
SI <hello@aleksei.email>
2024-12-06 09:49:46 +0300

    fix 0512

--- resources/views/_Wrap/FIX/milk/map.blade.php
+++ resources/views/_Wrap/FIX/milk/map.blade.php
@@ -1,10 +1,10 @@
-@component('_Wrap.FIX.wrap.FIX_on',['id' => $id, 'css_26071457' => $css_to_FIX ?? null])
+@component('_Wrap.FIX.wrap.FIX_on',['id' => $id, 'css' => $css_to_FIX ?? null, 'data' => $data ?? null])
     <div class="fix {{$fix_css ?? ''}}" data-close>
 
         <div class="_fix {{$css ?? ''}} {{$_fix_css ?? 'Pente aura W-100'}} / {{$cssMin ?? 'HWin / Jolt -t / Bg -gif'}} / yesEvents">
             <div class="Wings -mbl" style="z-index: 5">
-                <div class="_win / flex eC / noEvents / edge-Close">
-                    <div class="Close {{$css_Close ?? '-scroll -black -an'}} / yesEvents" data-close></div>
+                <div class="_win / flex eC / noEvents">
+                    <div class="Close {{$css_Close ?? 'vol-black -an'}} / yesEvents" data-close></div>
                 </div>
             </div>
             {!!$slot!!}
--- resources/views/_lego/map/preview.blade.php
+++ resources/views/_lego/map/preview.blade.php
@@ -1,4 +1,4 @@
-<div id="{{$id ?? 'map_preview'}}" class="Max -w5 / round shadow hidden / relative pointer" data-click data-goto='{{$goto ?? 'milk_map'}}' data-foo='click' corrector-jsscroll='100' onclick="ym({{config('AS.yandex_metrika_id')}},'reachGoal',$reachGoalName ?? 'click_btn_openMap')">
+<div id="{{$id ?? 'map_preview'}}" class="Max -w5 / round shadow hidden / relative pointer" data-click data-goto='{{$goto ?? 'milk_mapIndex'}}' data-foo='click' corrector-jsscroll='100' onclick="ym({{config('AS.yandex_metrika_id')}},'reachGoal',$reachGoalName ?? 'click_btn_openMap')">
     <img src="{{$img ?? '/images-0/map/ava.png'}}" alt="открыть карту">
     <div class="Veil">
         <div class="_bottom"></div>
