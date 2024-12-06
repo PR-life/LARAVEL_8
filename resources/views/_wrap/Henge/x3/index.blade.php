e7614f08175cf527eaaabe7ed02671cb3059c861
SI <hello@aleksei.email>
2024-12-06 10:08:01 +0300

    fix 0512

---
+++ resources/views/_wrap/Henge/x3/index.blade.php
@@ -0,0 +1,24 @@
+<div id="{{$id ?? 'div25111303'}}" class="Anime {{$css ?? ''}} / relative hidden [ padding top L ]">
+    <div class="Visible" visible-node='{{$id ?? 'div25111303'}}' visible-param='On'></div>
+    <div class="Henge aura / henge-anime anime-25111311 / Edge edge"> 
+        <div class="_wrap / Wings net ">
+            <div class="_win -t">
+                <div class="h2 -vw -lineHeight / {{$cssName ?? 'familyPragmatica'}}">
+                    {!!$name!!}
+                </div>
+            </div>
+        </div>
+        <div class="paragraphX2"></div>
+        <div class="Grid Henge / Void">
+            <div class="_1 _wrap / text slim void-m">
+                {!!$cell_1!!}
+            </div>
+            <div class="_2 _wrap text slim void-m">    
+                {!!$cell_2!!}        
+            </div>
+            <div class="_3 _wrap text slim void-m">
+                {!!$cell_3!!}
+            </div>
+        </div>
+    </div>
+</div>
