ced9ac5d20df455574199a9967d4eb2c92f777db
SI <hello@aleksei.email>
2024-12-13 10:41:18 +0300

    fix

--- resources/views/mod/Stories/wrap/lego/video.blade.php
+++ resources/views/mod/Stories/wrap/lego/video.blade.php
@@ -1,6 +1,6 @@
 @isset($video)
 <div class="Abs -all / flex cI">
-    <video class="yesEvents" {{$videoParam ?? ''}} muted autoplay loop aria-label="Видео о [описание]">
+    <video class="@empty($videoEvents) yesEvents @endempty" {{$videoParam ?? ''}} muted autoplay loop aria-label="Видео о [описание]">
         <source data-src="{{$video}}">
             Ваш браузер не поддерживает видео.
     </video>
